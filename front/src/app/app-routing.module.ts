2918ab2275afecbaad9c764719495055552d6888
Sayavong SOUANDARAVONG <sachaevans75@gmail.com>
2020-04-14 13:56:32 +0200

    update compo training-sessions-list

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
+
+import { HomeComponent } from './home/home.component';
 import { TrainingSessionsListComponent } from './training-sessions-list/training-sessions-list.component';
 
 const routes: Routes = [
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import {RouterModule} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
 import { TrainingSessionsListComponent } from './training-sessions-list/training-sessions-list.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
