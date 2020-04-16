b685c6068bf9d7f9fb2604af2c6ca11aead00f8c
Sayavong SOUANDARAVONG <sachaevans75@gmail.com>
2020-04-16 13:17:06 +0200

    ajout compo email-alert-settings

---
+++ front/src/app/email-alert-settings/email-alert-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-email-alert-settings',
+  templateUrl: './email-alert-settings.component.html',
+  styleUrls: ['./email-alert-settings.component.css']
+})
+export class EmailAlertSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
