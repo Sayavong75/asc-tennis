7586efc6e4eb8695d620ef42c20ae1927e405d2e
Sayavong SOUANDARAVONG <sachaevans75@gmail.com>
2020-04-17 17:13:41 +0200

    update compo training-sessions-list: ajout bouton inscription

--- front/src/app/training-sessions-list/training-sessions-list.component.ts
+++ front/src/app/training-sessions-list/training-sessions-list.component.ts
@@ -8,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class TrainingSessionsListComponent implements OnInit {
 
   constructor() { }
+  // Fonction pour modifier le bouton S'incrire/Se désinscrire : couleur et libellé'
+    // Valeurs par défaut
+    toggle = true;
+    status = 'S\'incrire';
 
+    registerRule() {
+      this.toggle = !this.toggle;
+      // Condition avec opérateur ternaire pour modifier le libellé du bouton (status)
+      this.status = this.toggle ? 'S\'incrire' : 'Se désinscire';
+    }
   ngOnInit(): void {
   }
 
