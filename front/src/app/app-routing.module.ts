0bee759066830bb2b32d5f49e2bf56e6f7872bb4
Sayavong SOUANDARAVONG <sachaevans75@gmail.com>
2020-04-20 17:46:18 +0200

    update routing et styles

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { TrainingSessionsListComponent } from './training-sessions-list/training-sessions-list.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { EmailAlertSettingsComponent } from './email-alert-settings/email-alert-settings.component';
-import { LoginComponent } from './login/login.component';
+import { AdminComponent} from './admin/admin.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'training-sessions-list', component: TrainingSessionsListComponent },
   { path: 'user-profile', component: UserProfileComponent },
-  { path: 'email-alert-settings', component: EmailAlertSettingsComponent }
+  { path: 'email-alert-settings', component: EmailAlertSettingsComponent },
+  { path: 'admin', component: AdminComponent }
 ];
 
 @NgModule({
