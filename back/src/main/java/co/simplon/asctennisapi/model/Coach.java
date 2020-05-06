e5aa8b2489918fd267b62c25c0ec07867c9b7441
Sayavong SOUANDARAVONG <sachaevans75@gmail.com>
2020-05-06 14:01:54 +0200

    ajout back entité Coach: controller, model, repo, service...

---
+++ back/src/main/java/co/simplon/asctennisapi/model/Coach.java
@@ -0,0 +1,105 @@
+package co.simplon.asctennisapi.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Coach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "coach_id_seq")
+    @SequenceGenerator(name = "coach_id_seq", sequenceName = "coach_id_seq", allocationSize = 1)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private String lastName;
+
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private String firstName;
+
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private String email1;
+
+    @Column(nullable = false)
+    private String email2;
+
+    @Column(nullable = false)
+    private String phoneNumber1;
+
+    @Column(nullable = false)
+    private String phoneNumber2;
+
+    @Column(nullable = false)
+    private boolean statusIsActive = true;
+
+    // GETTERS & SETTERS
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail1() {
+        return email1;
+    }
+
+    public void setEmail1(String email1) {
+        this.email1 = email1;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public void setEmail2(String email2) {
+        this.email2 = email2;
+    }
+
+    public String getPhoneNumber1() {
+        return phoneNumber1;
+    }
+
+    public void setPhoneNumber1(String phoneNumber1) {
+        this.phoneNumber1 = phoneNumber1;
+    }
+
+    public String getPhoneNumber2() {
+        return phoneNumber2;
+    }
+
+    public void setPhoneNumber2(String phoneNumber2) {
+        this.phoneNumber2 = phoneNumber2;
+    }
+
+    public boolean isStatusIsActive() {
+        return statusIsActive;
+    }
+
+    public void setStatusIsActive(boolean statusIsActive) {
+        this.statusIsActive = statusIsActive;
+    }
+}
--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- COACH CLUB
--- insert into coach(id, last_name, first_name, login, password, email1, email2, phone_number1, phone_number2, status_is_active) values (nextval('coach_id_seq'),'Boulanger','Gaston','gboulanger','Aew8uogo0','GastonBoulanger@armyspy.com','','01 47 87 12 78','','true');
--- insert into coach(id, last_name, first_name, login, password, email1, email2, phone_number1, phone_number2, status_is_active) values (nextval('coach_id_seq'),'Paradis','Jacques','jparadis','peeNae0upae','JacquesParadis@dayrep.com','','02 33 24 55 26','','true');
+insert into coach(id, last_name, first_name, email1, email2, phone_number1, phone_number2, status_is_active) values (nextval('coach_id_seq'),'Boulanger','Gaston','GastonBoulanger@armyspy.com','','01 47 87 12 78','','true');
+insert into coach(id, last_name, first_name, email1, email2, phone_number1, phone_number2, status_is_active) values (nextval('coach_id_seq'),'Paradis','Jacques','JacquesParadis@dayrep.com','','02 33 24 55 26','','true');
 
 -- DATA CLUB
 insert into club(id, name, address1, address2, zip_code, city, phone_number, iframe_google_maps, status_is_active) values (nextval('club_id_seq'),'Tennis Fruit défendu','82 bd Bellerive','','92500','Rueil-Malmaison','01 47 51 14 93', '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.2580225009383!2d2.1507863156748637!3d48.872357579288845!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb75b0e55581006dc!2sTennis%20du%20Fruit%20D%C3%A9fendu!5e0!3m2!1sfr!2sfr!4v1588677748571!5m2!1sfr!2sfr" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>', 'true');
